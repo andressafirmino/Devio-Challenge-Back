c2eea834536cccd0fb15b8ae9840f74d707f177b
andressafirmino <andressafirmino78@gmail.com>
2024-01-19 22:04:27 -0300

    feat: products repository created

--- src/products/products.repository.ts
+++ src/products/products.repository.ts
@@ -1,12 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
-import { ProductsRepository } from './products.repository';
 
 @Injectable()
-export class ProductsService {
-  constructor(private readonly productsRepository: ProductsRepository) { }
-  
+export class ProductsRepository {
   create(createProductDto: CreateProductDto) {
     return 'This action adds a new product';
   }
