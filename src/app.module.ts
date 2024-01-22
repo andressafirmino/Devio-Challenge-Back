bef8786c2d30d8b2a65dbea56647b1ef3da1b9be
andressafirmino <andressafirmino78@gmail.com>
2024-01-21 21:18:57 -0300

    feat: created payments CRUD feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductsModule } from './products/products.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [PrismaModule, ProductsModule],
+  imports: [PrismaModule, ProductsModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
